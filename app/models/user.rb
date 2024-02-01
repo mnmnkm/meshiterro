1e3c094944ca4096064a7dbf41b8c66056ddacf0
mnmnkm <152707508+mnmnkm@users.noreply.github.com>
2024-02-01 16:23:24 +0000

    [Add] アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :post_images, dependent: :destroy #Userが削除された時に、そのUserが投稿したPostImageが全て削除される
+  
 end
